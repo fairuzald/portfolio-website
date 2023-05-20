8a3be5f4e764eecbb20adc24002ee2b9804a159b
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-05-20 17:03:37 +0700

    feat(initialization): Inizialitation aos, react hotjar, tailwind lineclamp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,18 @@ import { type AppType } from "next/app";
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 200,
+    });
+  }, []);
+  return (<Component {...pageProps} />);
 };
 
 export default api.withTRPC(MyApp);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        width: "width",
+        height: "height",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
