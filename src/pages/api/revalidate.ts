20e226f1f26d7070a1b7485f684d3d055ea32ad5
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-05-27 02:20:28 +0700

    Prettier Code Revalidate

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -33,9 +33,7 @@ export default async function handler(
     // Revalidate Multiple Instance (if there is one)
     if (idToRevalidate && pageToRevalidate) {
       await res.revalidate(`/${pageToRevalidate}/${idToRevalidate}`);
-    } else if (idToRevalidate && !pageToRevalidate) {
-      await res.revalidate(`/${pageToRevalidate}`);
-    }
+    } 
     return res.status(200).json({ message: "Success revalidating" });
   } catch (err) {
     // Revalidate error
